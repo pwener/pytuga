bcd151e551265a6f19e059e9d3efbafa7c6455ae
Flavio Costa <Skingk.f@gmail.com>
2016-11-15 20:00:46 -0200

    Create vagrant file Signed-off-by: pwener<phwener@gmail.com> Signed-off-by: skingk<skingk.f@gmail.com>
    Conflicts:
    	.gitignore

---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo apt-get -y update
+sudo apt-get -y install python3-all python3-pyqt5 python3-pyqt5.qsci python3-pyqt5.qtsvg python3-pyqt5.qtwebkit python3-pip
+sudo pip3 install pytuga -U
+sudo pip3 install pytest
+
