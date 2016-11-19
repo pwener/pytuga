d7189e600a8d07f5bb1343c39166f69f247c0d77
pwener <phwener@gmail.com>
2016-11-19 16:44:03 -0200

    Installing local project

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
+# update source-list and install dependencies
 sudo apt-get -y update
-sudo apt-get -y install python3-all python3-pyqt5 python3-pyqt5.qsci python3-pyqt5.qtsvg python3-pyqt5.qtwebkit python3-pip
-sudo pip3 install pytuga -U
-sudo pip3 install pytest
-
+sudo apt-get -y install python3-all python3-pyqt5 python3-pyqt5.qsci python3-pyqt5.qtsvg python3-pyqt5.qtwebkit python3-pip git vim
+# clone and enter in repository
+git clone https://github.com/fabiommendes/pytuga.git
+cd pytuga
+# run setup.py to install local version
+sudo python3 setup.py install
